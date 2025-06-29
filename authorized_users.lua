6247edf1ba0b1d37e25c20ecfe81285874b2f6e8
BorysRoto <barytoko50@gmail.com>
2025-06-29 12:40:02 +0200

    Update authorized_users.lua

--- authorized_users.lua
+++ authorized_users.lua
@@ -1,7 +1,6 @@
 return {
   5709024479, -- Lunar / Thio
-  4857186048, -- testowe konto
-  7489424824, -- testowe konto2
+  2568715750, -- testowe konto
   2599202616, -- borytoko
   2645334884, -- polol
   3923431908, --polol2
