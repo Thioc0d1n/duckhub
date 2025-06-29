7328ada78754fd1685f1857ba6aebfb19761bf5b
BorysRoto <barytoko50@gmail.com>
2025-06-29 13:42:12 +0200

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -2,8 +2,6 @@ return {
   5709024479, -- Lunar / Thio
   2599202616, -- borytoko
   2645334884, -- polol
-  2568715750, -- borytoko15
   3923431908, --polol2
   7352719091, -- flocky
-  2568715750, -- testowe konto
 }
