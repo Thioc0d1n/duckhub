e3fa69cc9d505003bc81a71763318991652fb068
BorysRoto <barytoko50@gmail.com>
2025-07-16 17:07:05 +0200

    Update windycityauth.lua

--- windycityauth.lua
+++ windycityauth.lua
@@ -7,7 +7,7 @@ return {
   8351422663, -- solar
   4874515510, -- testcwel
   2405621620, -- yntnyc
-  8335253855, -- bailout
+  8336744312, -- bailout
   5656960725, -- ofcimturnt999
   8338030285, -- lunar
 }
