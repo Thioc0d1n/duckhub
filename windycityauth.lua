36710b7c71acb71d6b1d3b811cd4e01919252fb8
Thioc0d1n <93208847+Thioc0d1n@users.noreply.github.com>
2025-07-19 19:35:36 +0200

    Update windycityauth.lua

--- windycityauth.lua
+++ windycityauth.lua
@@ -7,12 +7,12 @@ return {
   8351422663, -- solar
   4874515510, -- testcwel
   1646558776, -- yntnyc
-  8253219675, -- bailout
+  8304331581, -- bailout
   7890995024, -- ofcimturnt999
   8338030285, -- lunar
   8762886541, -- Tnt_kay
   4952115911, -- topsmaccbd
   3514986793, -- yxngmoni
   5505182093, -- dc.0633
-  7723517515 -- zarskl
+  7723517515, -- zarskl
 }
